66eaca509ea2b1bc441bc52cdc49bbd0ed6d82fa
mskeerthu20 <mskeerthu20@gmail.com>
2020-05-26 14:04:11 +0530

    pushed successfully

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,20 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		
-	// Fill your code
-		
+	ConnectionManager conn = new ConnectionManager();
+	Connection conncheck = ConnectionManager.getConnection();
+	
+	if(conncheck!=null)
+		System.out.println("Connection Established");
+	else
+	    System.out.println("Check your connections");
+	
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,33 @@
 package utility;
 
-
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-	
-	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException
+	{
+		Connection conn = null;
+		Class.forName("oracle.jdbc.OracleDriver");
+		conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","prograd","oracle");
+		
+		if(conn != null)
+			System.out.println("Connection Established");
+		else
+			System.out.println("Connection is not Established");
+		return conn;
+		
+	}
+	 public static Properties loadPropertiesFile() throws IOException
+	 {
+		 Properties prop = new Properties();	
+			InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+			prop.load(in);
+			in.close(); 
+			return prop;
+		 
+	 }
 }
